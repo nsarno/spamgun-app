13095fcb128a78a4534c5adbacd0bd1295881ec0
Arnaud MESUREUR <arnaud.mesureur@gmail.com>
2015-06-28 20:52:46 +0200

    fix string replace mistake in Login.jsx

--- scripts/components/Login.jsx
+++ scripts/components/Login.jsx
@@ -36,7 +36,7 @@ export default class Login extends React.Component {
   }
 
   handleLogin() {
-    AuthStore.login(this.state.username, this.state.password);
+    LoginActions.login(this.state.username, this.state.password);
   }
 
   handleChange(event) {
